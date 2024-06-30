4a011e0c3f07b1df7d73c83f9ba45cec486a0a19
Venus Waikhom <apocalyse7@gmail.com>
2024-06-30 20:28:23 +0530

    first commit

---
+++ src/component/TextEditor.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { EditorState, convertToRaw } from "draft-js";
+import draftjsToHtml from "draftjs-to-html";
+import { Editor } from "react-draft-wysiwyg";
+
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+function TextEditor() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  const onEditorStateChange = (newEditorState) => {
+    setEditorState(newEditorState);
+  };
+
+  const rawContentState = convertToRaw(editorState.getCurrentContent());
+  const html = draftjsToHtml(rawContentState);
+  console.log(html);
+
+  return (
+    <div className="text-editor">
+      <div className="editor">
+        <Editor
+          editorState={editorState}
+          onEditorStateChange={onEditorStateChange}
+          wrapperClassName="wrapper-class"
+          editorClassName="editor-class"
+          toolbarClassName="toolbar-class"
+        />
+      </div>
+      <div className="text">Drafts to HTML</div>
+      <textarea className="text-area" disabled value={html}></textarea>
+    </div>
+  );
+}
+
+export default TextEditor;
