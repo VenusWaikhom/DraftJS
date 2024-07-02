7ad9e1e3b37ab31cb0d56975ec23259ce732aecb
Venus Waikhom <apocalyse7@gmail.com>
2024-07-02 20:33:33 +0530

    some changes

--- src/component/TextEditor.js
+++ src/component/TextEditor.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { EditorState, convertToRaw } from "draft-js";
 import draftjsToHtml from "draftjs-to-html";
 import { Editor } from "react-draft-wysiwyg";
-
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 function TextEditor() {
@@ -27,10 +26,12 @@ function TextEditor() {
           wrapperClassName="wrapper-class"
           editorClassName="editor-class"
           toolbarClassName="toolbar-class"
+          toolbar={{}}
         />
       </div>
       <div className="text">Drafts to HTML</div>
       <textarea className="text-area" disabled value={html}></textarea>
+      <div className="html-output" dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
 }
